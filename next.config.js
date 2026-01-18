2b9d8e4ac77e9d10e0180e687cff3754263fb11b
2836461019 <A2836461019@hotmail.com>
2026-01-19 01:56:31 +0800

    Configure CORS for API and add OSS image domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'example.com'],
+    domains: ['images.unsplash.com', 'example.com', 'restaurant-admin-nextjs.oss-cn-hangzhou.aliyuncs.com'],
+    remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: '**.aliyuncs.com',
+        },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+      },
+    ]
   },
 }
 
